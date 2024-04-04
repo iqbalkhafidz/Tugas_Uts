80c35f1495f8f8462bd3658bc79e2dec3d4548e2
iqbalkhafidz <151656255+iqbalkhafidz@users.noreply.github.com>
2024-04-04 09:57:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+    // Ambil elemen gambar profil
+    var profileImage = document.querySelector('.profile img');
+
+    // Tambahkan event listener untuk menangkap klik pada gambar profil
+    profileImage.addEventListener('click', function() {
+        alert('Ini adalah gambar profil saya!');
+    });
